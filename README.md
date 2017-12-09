069746e6daee838800dc0870b6b1bd8fb8696117
Owais <owaiswiz@gmail.com>
2017-12-10 03:35:55 +0530

    Fix expected float result, expect exception in correct manner
    * Also expect InvalidReadingError instead of InvalidFileSpecifiedError when the file specified is correct but the reading is inaccurate
    * Update Readme

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# Ds18b20
+NOTE: In Development
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/ds18b20`. To experiment with that code, run `bin/console` for an interactive prompt.
+[![Build Status](https://travis-ci.org/owaiswiz/ds18b20.svg?branch=master)](https://travis-ci.org/owaiswiz/ds18b20)
 
-TODO: Delete this and the text above, and describe your gem
+# DS18B20
+
+This gem makes interfacing with the DS18B20 temperature easier over 1-Wire.
+
+
+After setting it up correctly make sure you have the correct file path where it's output is being recorded.
+The file name on raspberry pi should be `w1_slave`. If you don't know where the file is located - please read : [DS18B20 - Temperature Sensing](https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing?view=all).
+
+
+After making sure that you have the file path and that output looks similar to one in the tutorial above, you can easily use this gem following the instructions below (not yet released - soon).
 
 ## Installation
 
@@ -22,13 +31,11 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+In-development
 
 ## Development
 
 After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
-
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
 ## Contributing
 
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 RSpec.describe Ds18b20::Parser do
 
   let(:valid_sample_positive_24_187C) { described_class.new support_file_location("valid_sample_positive_24_187C") }
-  let(:valid_sample_negative_11_440C) { described_class.new support_file_location("valid_sample_negative_11_440C") }
+  let(:valid_sample_negative_10_125C) { described_class.new support_file_location("valid_sample_negative_10_125C") }
   let(:invalid_sample_1)              { described_class.new support_file_location("invalid_sample_1") }
-  let(:invalid_sample_2)              { described_class.new support_file_location("invalid_sample_2") }
-    
+  let(:invalid_reading_sample_1)              { described_class.new support_file_location("invalid_reading_sample_1") }
+  let(:invalid_reading_sample_2)              { described_class.new support_file_location("invalid_reading_sample_2") }
+
   context "there is a valid file with a positive temperature" do
     describe ".get_temperature_celsius" do
       it "returns the correct temperature" do
@@ -16,13 +17,13 @@ RSpec.describe Ds18b20::Parser do
 
     describe ".get_temperature_fahrenheit" do
       it "returns the correct temperature" do
-        expect(valid_sample_positive_24_187C.get_temperature_fahrenheit).to eq(75.536)
+        expect(valid_sample_positive_24_187C.get_temperature_fahrenheit).to eq(75.5375)
       end
     end
     
     describe ".get_temperature_kelvin" do
       it "returns the correct temperature" do
-        expect(valid_sample_positive_24_187C.get_temperature_kelvin).to eq(297.337)
+        expect(valid_sample_positive_24_187C.get_temperature_kelvin).to eq(297.3375)
       end
     end
   end
@@ -30,19 +31,19 @@ RSpec.describe Ds18b20::Parser do
   context "there is a valid file with a negative temperature" do
     describe ".get_temperature_celsius" do
       it "returns the correct temperature" do
-        expect(valid_sample_negative_11_440C.get_temperature_celsius).to eq(-11.440)
+        expect(valid_sample_negative_10_125C.get_temperature_celsius).to eq(-10.125)
       end
     end
 
     describe ".get_temperature_fahrenheit" do
       it "returns the correct temperature" do
-        expect(valid_sample_negative_11_440C.get_temperature_fahrenheit).to eq(11.408)
+        expect(valid_sample_negative_10_125C.get_temperature_fahrenheit.round(3)).to eq(13.775)
       end
     end
     
     describe ".get_temperature_kelvin" do
       it "returns the correct temperature" do 
-        expect(valid_sample_negative_11_440C.get_temperature_kelvin).to eq(261.71)
+        expect(valid_sample_negative_10_125C.get_temperature_kelvin).to eq(263.025)
       end
     end
   end
@@ -50,22 +51,25 @@ RSpec.describe Ds18b20::Parser do
   context "there is an invalid file" do
     describe ".get_temperature_celsius" do
       it "raises an invalid file error" do
-        expect(invalid_sample_1.get_temperature_celsius).to raise_error Ds18b20::InvalidFileSpecifiedError
-        expect(invalid_sample_2.get_temperature_celsius).to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect { invalid_sample_1.get_temperature_celsius }.to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect { invalid_reading_sample_1.get_temperature_celsius }.to raise_error Ds18b20::InvalidReadingError
+        expect { invalid_reading_sample_2.get_temperature_celsius }.to raise_error Ds18b20::InvalidReadingError
       end
     end
     
     describe ".get_temperature_fahrenheit" do
       it "raises an invalid file error" do
-        expect(invalid_sample_1.get_temperature_fahrenheit).to raise_error Ds18b20::InvalidFileSpecifiedError
-        expect(invalid_sample_2.get_temperature_fahrenheit).to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect { invalid_sample_1.get_temperature_fahrenheit }.to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect { invalid_reading_sample_1.get_temperature_fahrenheit }.to raise_error Ds18b20::InvalidReadingError
+        expect { invalid_reading_sample_2.get_temperature_fahrenheit }.to raise_error Ds18b20::InvalidReadingError
       end
     end
     
     describe ".get_temperature_kelvin" do
       it "raises an invalid file error" do
-        expect(invalid_sample_1.get_temperature_kelvin).to raise_error Ds18b20::InvalidFileSpecifiedError
-        expect(invalid_sample_2.get_temperature_kelvin).to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect { invalid_sample_1.get_temperature_kelvin }.to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect { invalid_reading_sample_1.get_temperature_kelvin }.to raise_error Ds18b20::InvalidReadingError
+        expect { invalid_reading_sample_2.get_temperature_kelvin }.to raise_error Ds18b20::InvalidReadingError
       end
     end
   end
