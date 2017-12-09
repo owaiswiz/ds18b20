580130abdb604a3bcd2cdde5ef94c86ca2025d61
Owais <owaiswiz@gmail.com>
2017-12-10 02:38:21 +0530

    Compare value instead of object identity using eq instead of equal
    * Use eq instead of equal
    * Change the class of exceptions that are expected

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe Ds18b20::Parser do
   context "there is a valid file with a positive temperature" do
     describe ".get_temperature_celsius" do
       it "returns the correct temperature" do
-        expect(valid_sample_positive_24_187C.get_temperature_celsius).to equal(24.187)
+        expect(valid_sample_positive_24_187C.get_temperature_celsius).to eq(24.1875)
       end
     end
 
     describe ".get_temperature_fahrenheit" do
       it "returns the correct temperature" do
-        expect(valid_sample_positive_24_187C.get_temperature_fahrenheit).to equal(75.536)
+        expect(valid_sample_positive_24_187C.get_temperature_fahrenheit).to eq(75.536)
       end
     end
     
     describe ".get_temperature_kelvin" do
       it "returns the correct temperature" do
-        expect(valid_sample_positive_24_187C.get_temperature_kelvin).to equal(297.337)
+        expect(valid_sample_positive_24_187C.get_temperature_kelvin).to eq(297.337)
       end
     end
   end
@@ -30,19 +30,19 @@ RSpec.describe Ds18b20::Parser do
   context "there is a valid file with a negative temperature" do
     describe ".get_temperature_celsius" do
       it "returns the correct temperature" do
-        expect(valid_sample_negative_11_440C.get_temperature_celsius).to equal(-11.440)
+        expect(valid_sample_negative_11_440C.get_temperature_celsius).to eq(-11.440)
       end
     end
 
     describe ".get_temperature_fahrenheit" do
       it "returns the correct temperature" do
-        expect(valid_sample_negative_11_440C.get_temperature_fahrenheit).to equal(11.408)
+        expect(valid_sample_negative_11_440C.get_temperature_fahrenheit).to eq(11.408)
       end
     end
     
     describe ".get_temperature_kelvin" do
       it "returns the correct temperature" do 
-        expect(valid_sample_negative_11_440C.get_temperature_kelvin).to equal(261.71)
+        expect(valid_sample_negative_11_440C.get_temperature_kelvin).to eq(261.71)
       end
     end
   end
@@ -50,22 +50,22 @@ RSpec.describe Ds18b20::Parser do
   context "there is an invalid file" do
     describe ".get_temperature_celsius" do
       it "raises an invalid file error" do
-        expect(invalid_sample_1.get_temperature_celsius).to raise_error Ds18b20::InvalidFileSpecified
-        expect(invalid_sample_2.get_temperature_celsius).to raise_error Ds18b20::InvalidFileSpecified
+        expect(invalid_sample_1.get_temperature_celsius).to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect(invalid_sample_2.get_temperature_celsius).to raise_error Ds18b20::InvalidFileSpecifiedError
       end
     end
     
     describe ".get_temperature_fahrenheit" do
       it "raises an invalid file error" do
-        expect(invalid_sample_1.get_temperature_fahrenheit).to raise_error Ds18b20::InvalidFileSpecified
-        expect(invalid_sample_2.get_temperature_fahrenheit).to raise_error Ds18b20::InvalidFileSpecified
+        expect(invalid_sample_1.get_temperature_fahrenheit).to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect(invalid_sample_2.get_temperature_fahrenheit).to raise_error Ds18b20::InvalidFileSpecifiedError
       end
     end
     
     describe ".get_temperature_kelvin" do
       it "raises an invalid file error" do
-        expect(invalid_sample_1.get_temperature_kelvin).to raise_error Ds18b20::InvalidFileSpecified
-        expect(invalid_sample_2.get_temperature_kelvin).to raise_error Ds18b20::InvalidFileSpecified
+        expect(invalid_sample_1.get_temperature_kelvin).to raise_error Ds18b20::InvalidFileSpecifiedError
+        expect(invalid_sample_2.get_temperature_kelvin).to raise_error Ds18b20::InvalidFileSpecifiedError
       end
     end
   end
